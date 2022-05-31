5d1b4a93c54c7c13926400516fadadde9bacc730
jsanjak <jsanjak@gmail.com>
2022-05-31 11:08:36 -0400

    example of push KGX to neo4j

---
+++ kgx-neo4j.sh
@@ -0,0 +1,7 @@
+kgx neo4j-upload --uri bolt://localhost:7687 \
+                 --username neo4j \
+                 --password password \
+                 --input-format tsv \
+                 data/kgx_test_nodes.tsv data/kgx_test_edges.tsv
+
+
